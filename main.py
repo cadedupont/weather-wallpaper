c166d392fa4a11dcda3b426368f90e3baf18a4d9
@cadedupont <cadedupont45@gmail.com>
2023-05-21 12:11:08 -0500

    Add time of day to search query

--- main.py
+++ main.py
@@ -3,30 +3,35 @@ import os
 from unsplash.api import Api
 from unsplash.auth import Auth
 
-# Get access key, secret key adn redirect uri from environment variables for unsplash access
-unsplash_access = os.getenv('UNSPLASH_ACCESS_KEY')
-unsplash_secret = os.getenv('UNSPLASH_SECRET_KEY')
-unsplash_uri = os.getenv('UNSPLASH_REDIRECT_URI')
+# Create instance of Unsplash API
+unsplash_api = Api(Auth(os.getenv('UNSPLASH_ACCESS_KEY'), os.getenv('UNSPLASH_SECRET_KEY'), os.getenv('UNSPLASH_REDIRECT_URI')))
 
-# Create an instance of the Auth and Api classes
-unsplash_api = Api(Auth(unsplash_access, unsplash_secret, unsplash_uri))
-
-# Get access key for OpenWeatherMap from environment variables
+# Get access key for OpenWeatherMap API from environment variables
 weather_key = os.getenv('WEATHER_KEY')
 
 # Convert Kelvin to Fahrenheit
 def from_kelvin_to_fahrenheit(kelvin):
     return (kelvin - 273.15) * 9/5 + 32
 
-# Get weather description and temperature from OpenWeatherMap API
-def get_weather_desc():
+def get_search_query():
+    # Create base URL for OpenWeatherMap API, hard-coding city ID for Fayetteville, AR
     url = 'http://api.openweathermap.org/data/2.5/weather?id=4110486&appid=' + weather_key
+
+    # Get response from OpenWeatherMap API, marshal to JSON
     response = requests.get(url).json()
-    return response['weather'][0]['description'] + ' and ' + str(round(from_kelvin_to_fahrenheit(response['main']['temp']), 2)) + ' degrees Fahrenheit'
+
+    # Get weather description, current temperature, and time of day
+    description = response['weather'][0]['description']
+    temp = int(round(from_kelvin_to_fahrenheit(response['main']['temp']), 0))
+    time = 'daytime' if response['dt'] > response['sys']['sunrise'] and response['dt'] < response['sys']['sunset'] else 'nighttime'
+
+    # Return search query
+    return description + ' at ' + str(temp) + ' degrees Fahrenheit outside during the ' + time
 
 def main():
     # Generate image url from Unsplash API
-    image_url = unsplash_api.search.photos(get_weather_desc(), page=1, per_page=1)['results'][0].urls.raw
+    # Not enough variation in images produced by query, so choosing random image from results
+    image_url = unsplash_api.photo.random(query=get_search_query())[0].urls.raw
 
     # Download image
     open('image.jpg', 'wb').write(requests.get(image_url).content)
