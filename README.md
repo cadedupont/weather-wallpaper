455dd5cf3f04a0f3de78d96441f156c327568ad5
@cadedupont <cadedupont45@gmail.com>
2023-05-21 14:48:42 -0500

    Update README, minor changes

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # weather-photo-frame
-Python script for gathering weather data from OpenWeatherMap API, using as a search query to download an image from Unsplash to display to a digital photo frame.<br>
+Python script for gathering weather data from OpenWeatherMap API, using as a search query to download an image from Unsplash to set as desktop wallpaper.<br>
 
-Must configure environment variables for API access keys before use.<br>
+Currently has limited functionality. Requires installing various dependencies, adding private API keys to the system's environment variables, and is only configured to change the desktop wallpaper of MacOS devices.
 
-Upon running the script, the script will continue to run until a change is detected in the current state of the weather in Fayetteville, AR, upon which a new image will be generated.
+## To run
+Because the script uses an infinite loop to detect changes in the weather, the script must run in the background. This can be done using `nohup`:<br>
+
+`nohup /path/to/main.py > output.log &`<br>
+
+This will run the Python script in the background, piping what's printed by the script to the `output.log` file.<br>
+
+To stop the process, run the following command in the terminal to find the process ID:<br>
+
+`ps ax | grep main.py`<br>
+
+The ID of a process is denoted by the 5-digit number found at the beginning of a new line output by the command. Then, use the `kill` command to stop the process:<br>
+
+`kill id`<br>
+
+Replace 'id' with the 5-digit number found previously.
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+#! /opt/homebrew/bin/python3
+
 import requests
 import os
+from appscript import app, mactypes
+from datetime import datetime
 from unsplash.api import Api
 from unsplash.auth import Auth
 
-# Create instance of Unsplash API, get OpenWeatherMap API key from environment variables
-unsplash = Api(Auth(os.getenv('UNSPLASH_ACCESS_KEY'), os.getenv('UNSPLASH_SECRET_KEY'), os.getenv('UNSPLASH_REDIRECT_URI')))
-WEATHER_KEY = os.getenv('WEATHER_KEY')
+# Create instance of Unsplash API, get URL for current weather
+unsplash_api = Api(Auth(os.getenv('UNSPLASH_ACCESS_KEY'), os.getenv('UNSPLASH_SECRET_KEY'), os.getenv('UNSPLASH_REDIRECT_URI')))
+weather_url = f'http://api.openweathermap.org/data/2.5/weather?id=4110486&appid={os.getenv("WEATHER_KEY")}'
 
 # Convert Kelvin to Fahrenheit
 def kelvin_to_fahrenheit(kelvin):
     return (kelvin - 273.15) * 9/5 + 32
 
+# Get search query for Unsplash API
 def get_search_query(weather):
-    # Get weather description, current temperature
+    # Get weather description and temperature as an integer
     description = weather['weather'][0]['description']
     temp = int(round(kelvin_to_fahrenheit(weather['main']['temp']), 0))
-    
-    # Get temperature description
+
+    # Based on temperature, get temperature description
     if temp < 32:
         temp_desc = 'freezing'
     elif temp < 50:
@@ -28,28 +33,35 @@ def get_search_query(weather):
     else:
         temp_desc = 'hot'
 
-    # Get time of day
+    # Get time of day description based on whether current time is between sunrise and sunset
     time = 'day' if weather['dt'] > weather['sys']['sunrise'] and weather['dt'] < weather['sys']['sunset'] else 'night'
 
     # Return search query
     return description + ' and ' + temp_desc + ' outside during the ' + time
 
-def main():
-    # Get weather from OpenWeatherMap API, marshal to JSON
-    url = f'http://api.openweathermap.org/data/2.5/weather?id=4110486&appid={WEATHER_KEY}'
-    curr_weather = requests.get(url).json()
+# Get current weather
+def get_weather():
+    return requests.get(weather_url).json()
 
-    # Get image url from Unsplash API and download to file
-    image_url = unsplash.photo.random(query=get_search_query(curr_weather))[0].urls.raw
+# Get image url from Unsplash API and download to file, set wallpaper
+def set_wallpaper(weather):
+    image_url = unsplash_api.photo.random(query=get_search_query(weather))[0].urls.raw
     open('image.jpg', 'wb').write(requests.get(image_url).content)
+    app('Finder').desktop_picture.set(mactypes.File('./image.jpg'))
 
-    # Update image if new weather is different from old weather
+def main():
+    # Get current weather and set wallpaper
+    curr_weather = get_weather()
+    set_wallpaper(curr_weather)
+
+    # Continuously check for new weather, if new weather is different, print time of change and set new wallpaper
     while True:
-        new_weather = requests.get(url).json()
+        new_weather = get_weather()
         if get_search_query(new_weather) != get_search_query(curr_weather):
-            image_url = unsplash.photo.random(query=get_search_query(new_weather))[0].urls.raw
-            open('image.jpg', 'wb').write(requests.get(image_url).content)
+            print('New weather detected on ' + str(datetime.now().date()) + ' at ' + str(datetime.now().time()))
+            set_wallpaper(new_weather)
             curr_weather = new_weather
 
+# If this file is run directly, call main function
 if __name__ == '__main__':
     main()
