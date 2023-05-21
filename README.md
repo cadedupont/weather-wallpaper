c8fb519e4ff0b702e7c1c794c33852c2863091ef
@cadedupont <cadedupont45@gmail.com>
2023-05-21 12:50:14 -0500

    Simplify search query

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # weather-photo-frame
 Python script for gathering weather data from OpenWeatherMap API, using as a search query to download an image from Unsplash to display to a digital photo frame.<br>
 
-Must configure environment variables for API access keys before use.
+Must configure environment variables for API access keys before use.<br>
+
+Upon running the script, the script will continue to run until a change is detected in the current state of the weather in Fayetteville, AR, upon which a new image will be generated.
--- main.py
+++ main.py
@@ -3,38 +3,53 @@ import os
 from unsplash.api import Api
 from unsplash.auth import Auth
 
-# Create instance of Unsplash API
-unsplash_api = Api(Auth(os.getenv('UNSPLASH_ACCESS_KEY'), os.getenv('UNSPLASH_SECRET_KEY'), os.getenv('UNSPLASH_REDIRECT_URI')))
-
-# Get access key for OpenWeatherMap API from environment variables
-weather_key = os.getenv('WEATHER_KEY')
+# Create instance of Unsplash API, get OpenWeatherMap API key from environment variables
+unsplash = Api(Auth(os.getenv('UNSPLASH_ACCESS_KEY'), os.getenv('UNSPLASH_SECRET_KEY'), os.getenv('UNSPLASH_REDIRECT_URI')))
+WEATHER_KEY = os.getenv('WEATHER_KEY')
 
 # Convert Kelvin to Fahrenheit
-def from_kelvin_to_fahrenheit(kelvin):
+def kelvin_to_fahrenheit(kelvin):
     return (kelvin - 273.15) * 9/5 + 32
 
-def get_search_query():
-    # Create base URL for OpenWeatherMap API, hard-coding city ID for Fayetteville, AR
-    url = 'http://api.openweathermap.org/data/2.5/weather?id=4110486&appid=' + weather_key
+def get_search_query(weather):
+    # Get weather description, current temperature
+    description = weather['weather'][0]['description']
+    temp = int(round(kelvin_to_fahrenheit(weather['main']['temp']), 0))
+    
+    # Get temperature description
+    if temp < 32:
+        temp_desc = 'freezing'
+    elif temp < 50:
+        temp_desc = 'cold'
+    elif temp < 70:
+        temp_desc = 'cool'
+    elif temp < 80:
+        temp_desc = 'warm'
+    else:
+        temp_desc = 'hot'
 
-    # Get response from OpenWeatherMap API, marshal to JSON
-    response = requests.get(url).json()
-
-    # Get weather description, current temperature, and time of day
-    description = response['weather'][0]['description']
-    temp = int(round(from_kelvin_to_fahrenheit(response['main']['temp']), 0))
-    time = 'daytime' if response['dt'] > response['sys']['sunrise'] and response['dt'] < response['sys']['sunset'] else 'nighttime'
+    # Get time of day
+    time = 'day' if weather['dt'] > weather['sys']['sunrise'] and weather['dt'] < weather['sys']['sunset'] else 'night'
 
     # Return search query
-    return description + ' at ' + str(temp) + ' degrees Fahrenheit outside during the ' + time
+    return description + ' and ' + temp_desc + ' outside during the ' + time
 
 def main():
-    # Generate image url from Unsplash API
-    # Not enough variation in images produced by query, so choosing random image from results
-    image_url = unsplash_api.photo.random(query=get_search_query())[0].urls.raw
+    # Get weather from OpenWeatherMap API, marshal to JSON
+    url = f'http://api.openweathermap.org/data/2.5/weather?id=4110486&appid={WEATHER_KEY}'
+    curr_weather = requests.get(url).json()
 
-    # Download image
+    # Get image url from Unsplash API and download to file
+    image_url = unsplash.photo.random(query=get_search_query(curr_weather))[0].urls.raw
     open('image.jpg', 'wb').write(requests.get(image_url).content)
+
+    # Update image if new weather is different from old weather
+    while True:
+        new_weather = requests.get(url).json()
+        if get_search_query(new_weather) != get_search_query(curr_weather):
+            image_url = unsplash.photo.random(query=get_search_query(new_weather))[0].urls.raw
+            open('image.jpg', 'wb').write(requests.get(image_url).content)
+            curr_weather = new_weather
 
 if __name__ == '__main__':
     main()
